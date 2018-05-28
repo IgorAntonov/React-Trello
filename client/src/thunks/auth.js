80db3b14f1b1a356eb444fb62a1ad2aa57c7f56e
Igor Antonov <igorr1496@gmail.com>
2018-05-28 15:17:40 +0200

    client/ refactor auth reducer

--- client/src/thunks/auth.js
+++ client/src/thunks/auth.js
@@ -2,18 +2,18 @@ import { actions } from '../reducers/auth';
 import { signupUser, fetchCurrentUser, loginUser } from './api';
 
 export const signup = ({ email, password, name }) => async dispatch => {
-  dispatch(actions.requestSignup());
+  dispatch(actions.requestApi());
   try {
     const { data } = await signupUser(email, password, name);
-    dispatch(actions.successSignup(data));
+    dispatch(actions.successCurrentUser(data));
   } catch (err) {
     const { error } = err.response.data;
-    dispatch(actions.failureSignup(error));
+    dispatch(actions.failureAuth(error));
   }
 };
 
 export const fetchUser = () => async dispatch => {
-  dispatch(actions.requestCurrentUser());
+  dispatch(actions.requestApi());
   try {
     const { data } = await fetchCurrentUser();
     dispatch(actions.successCurrentUser(data));
@@ -23,12 +23,12 @@ export const fetchUser = () => async dispatch => {
 };
 
 export const login = ({ email, password }) => async dispatch => {
-  dispatch(actions.requestLogin());
+  dispatch(actions.requestApi());
   try {
     const { data } = await loginUser(email, password);
-    dispatch(actions.successLogin(data));
+    dispatch(actions.successCurrentUser(data));
   } catch (err) {
     const { data } = err.response;
-    dispatch(actions.failureLogin(data));
+    dispatch(actions.failureAuth(data));
   }
 };
