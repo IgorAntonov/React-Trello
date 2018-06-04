b069c1662230320f5c7e9361e734e33c6c861fb9
Igor Antonov <igorr1496@gmail.com>
2018-06-04 11:18:54 +0200

    client/feat/ WithLoading RenderProp Component

--- client/src/components/App/container.js
+++ client/src/components/App/container.js
@@ -5,7 +5,8 @@ import { fetchUser } from 'Src/thunks/auth';
 import { HotApp } from './App';
 
 const mapStateToProps = state => ({
-  isAuth: state.auth.isAuthenticated
+  isAuth: state.auth.isAuthenticated,
+  isLoading: state.auth.isLoading
 });
 
 export const AppContainer = withRouter(connect(mapStateToProps, { fetchUser })(HotApp));
