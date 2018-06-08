7e25808221beefc32728e350d4790024fd819520
Igor Antonov <igorr1496@gmail.com>
2018-06-08 11:38:12 +0200

    server/feat/ Add theme key to User Models

--- server/models/GoogleUser.js
+++ server/models/GoogleUser.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const userGoogleSchema = new mongoose.Schema({
   googleId: { type: String, required: true },
-  name: { type: String, required: true }
+  name: { type: String, required: true },
+  theme: { type: String, required: true, default: 'default' }
 });
 
 module.exports = mongoose.model('GoogleUser', userGoogleSchema);
