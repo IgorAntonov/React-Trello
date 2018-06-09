c1d637f9052f9d097f49008b129f292084b9db40
Igor Antonov <igorr1496@gmail.com>
2018-06-09 14:35:59 +0200

    server/feat/ theme controller and route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const passport = require('passport');
 
-const router = express.Router();
-
 const auth = require('../controllers/auth');
-
 require('../services/passport');
+
+const router = express.Router();
 
 router.post(
   '/login',
