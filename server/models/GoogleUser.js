d89d0db4a5cb0438817a2bfde49ef673bef1c81e
Igor Antonov <igorr1496@gmail.com>
2018-06-16 09:45:10 +0200

    server/refactor/ Board model and controller

--- server/models/GoogleUser.js
+++ server/models/GoogleUser.js
@@ -6,6 +6,7 @@ const userGoogleSchema = Schema({
   googleId: { type: String, required: true },
   name: { type: String, required: true },
   theme: { type: String, required: true, default: 'default' },
+  __v: { type: Number, select: false },
   boards: [{ type: Schema.Types.ObjectId, ref: 'Board' }]
 });
 
