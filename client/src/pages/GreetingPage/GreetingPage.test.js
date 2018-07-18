a106b0a310aac40016bc2d7026a0d60403e97891
Igor Antonov <igorr1496@gmail.com>
2018-07-18 12:32:01 +0200

    client/feat/ tests for pages

--- client/src/pages/GreetingPage/GreetingPage.test.js
+++ client/src/pages/GreetingPage/GreetingPage.test.js
@@ -1,38 +1,53 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import { Footer, StyledLink as Link } from 'Src/ui';
+import { Footer, Header, StyledLink } from 'Src/ui';
+import { AuthBlock } from 'Src/features/AuthBlock';
 import { GreetingPage } from './GreetingPage';
-import { Header, Logo } from './style';
+import { Logo } from './style';
 
-describe('<GreetingPage />', () => {
+describe('<GreetingPage/>', () => {
   let wrapper;
+  const props = {
+    isAuth: false,
+    logoutUser: jest.fn()
+  };
+
   beforeEach(() => {
-    wrapper = shallow(<GreetingPage />);
+    wrapper = shallow(<GreetingPage {...props} />);
   });
 
   test('renders', () => {
     expect(wrapper.exists()).toBe(true);
   });
 
-  test('should render <Header />', () => {
-    expect(wrapper.find(Header)).toHaveLength(1);
+  test('should render <Header/>', () => {
+    expect(wrapper.contains(<Header />)).toBe(true);
   });
 
-  test('should render <Footer />', () => {
-    expect(wrapper.find(Footer)).toHaveLength(1);
+  test('should render <Footer/>', () => {
+    expect(wrapper.contains(<Footer />)).toBe(true);
   });
 
-  test('should render <Link /> to signup page', () => {
-    const link = wrapper.find(Link);
+  test('should render <AuthBlock/>', () => {
+    expect(wrapper.contains(<AuthBlock />)).toBe(true);
+  });
 
+  test('should render <Link/> to signup page, when user is not authenticated', () => {
+    const link = wrapper.find(StyledLink);
     expect(link).toHaveLength(1);
     expect(link.prop('to')).toEqual('/signup');
   });
 
-  test('should render <Logo /> with link', () => {
-    const logo = wrapper.find(Logo);
+  test('should render <Link/> to boards page, when user is authenticated', () => {
+    wrapper.setProps({ isAuth: true });
+    const link = wrapper.find(StyledLink);
+    expect(link).toHaveLength(1);
+    expect(link.prop('to')).toEqual('/boards');
+  });
 
+  test('should render <Logo/> with link', () => {
+    const logo = wrapper.find(Logo);
     expect(logo).toHaveLength(1);
     expect(logo.prop('to')).toEqual('/');
   });
