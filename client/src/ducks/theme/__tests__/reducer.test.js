ae587e3d04c6b02afca76bb1dca8be4738a7dbed
Igor Antonov <igorr1496@gmail.com>
2018-07-21 16:13:22 +0200

    client/feat/ tests for theme duck

---
+++ client/src/ducks/theme/__tests__/reducer.test.js
@@ -0,0 +1,40 @@
+import { themes } from 'Src/helpers';
+import { reducer, initialState } from '../reducer';
+import { actions } from '../actions';
+
+describe('theme reducer', () => {
+  test('should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual(initialState);
+  });
+
+  test('should handle THEME_REQUEST', () => {
+    expect(reducer(initialState, actions.requestTheme()))
+      .toEqual({
+        ...initialState,
+        isLoading: true
+      });
+  });
+
+  test('should handle THEME_SUCCESS', () => {
+    const payload = 'green';
+    expect(reducer(initialState, actions.successTheme(payload)))
+      .toEqual({
+        ...initialState,
+        name: payload,
+        colors: { ...themes[payload] },
+        isLoading: false
+      });
+  });
+
+  test('should handle THEME_FAILURE', () => {
+    const payload = 'error';
+    expect(reducer(initialState, actions.failureTheme(payload)))
+      .toEqual({
+        ...initialState,
+        name: 'default',
+        colors: { ...themes.default },
+        isLoading: false,
+        error: payload
+      });
+  });
+});
