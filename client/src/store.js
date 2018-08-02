59f4b7dd257998c7388ecb503132414d2642bc7d
Igor Antonov <igorr1496@gmail.com>
2018-08-02 17:19:03 +0200

    client/fix/ redux-logger only for dev

--- client/src/store.js
+++ client/src/store.js
@@ -4,9 +4,14 @@ import thunkMiddleware from 'redux-thunk';
 
 import { rootReducer } from 'Src/ducks';
 
-const logger = createLogger({ collapsed: true });
+const middlewares = [thunkMiddleware];
+
+if (process.env.NODE_ENV === 'development') {
+  const logger = createLogger({ collapsed: true });
+  middlewares.push(logger);
+}
 
 export const store = createStore(
   rootReducer,
-  applyMiddleware(thunkMiddleware, logger)
+  applyMiddleware(...middlewares)
 );
